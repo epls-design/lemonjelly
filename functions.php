fa7fad46da1a52da2e3b13c3b097d7300ea80ffa
Hus <hus@epls.design>
2024-04-03 12:07:31 +0100

    version numbers changed

--- functions.php
+++ functions.php
@@ -30,16 +30,15 @@ add_action('admin_init', 'wpdocs_theme_add_editor_styles', 500);
 
 
 function ezpzconsultations_enqueue_styles() {
-  $theme = wp_get_theme();
-
-  $version = $theme->get('Version');
-
-  wp_enqueue_style(
-    'ezpz-consultations-style',
-    get_stylesheet_directory_uri() . '/style.css',
-    array('jellypress-styles'),
-    $version // TODO: FILEMTIMES
-  );
+  $style_css_path = get_stylesheet_directory() . '/style.css';
+  if (file_exists($style_css_path)) {
+    wp_enqueue_style(
+      'ezpz-consultations-style',
+      get_stylesheet_directory_uri() . '/style.css',
+      array('jellypress-styles'),
+      filemtime($style_css_path)
+    );
+  }
 
   $consultation_css_path = get_stylesheet_directory() . '/consultation.css';
 
@@ -51,14 +50,16 @@ function ezpzconsultations_enqueue_styles() {
       filemtime($consultation_css_path)
     );
   }
+  $theme_min_path = get_stylesheet_directory() . '/style.css';
 
-  wp_enqueue_script(
-    'ezpz-consultations-theme',
-    get_stylesheet_directory_uri() . '/js/theme.min.js',
-    array('jellypress-scripts'),
-    $version,
-    true
-  );
+  if (file_exists($theme_min_path)) {
+    wp_enqueue_script(
+      'ezpz-consultations-theme',
+      get_stylesheet_directory_uri() . '/js/theme.min.js',
+      array('jellypress-scripts'),
+      filemtime($theme_min_path)
+    );
+  }
 }
 
 add_action('acf/init', 'ezpzconsultations_register_blocks', 20);
