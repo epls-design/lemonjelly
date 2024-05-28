5a6e129e0a4dbf6514cb37f09c2958e6837b8e46
Matt Weet <matt@epls.design>
2024-05-28 11:13:18 +0100

    Add filters + a little tidy up

--- blocks/feedback-map/scripts.js
+++ blocks/feedback-map/scripts.js
@@ -3,91 +3,105 @@
 	// TODO Add esc e vent listener, if add Feedback is active, close it
 	// TODO: Add intro screen
 	// TODO: ADD CLUSTERING
-	// TODO: ADD filters
 	// TODO: On Gravity Form submit, close the feedback form and add a marker to the map opening the infowindow
 
-	function addUserFeedback(map) {
-		// Find .map-interactivity button and add an event listener
+	/********************************************
+	 * PLOT MARKERS
+	 ********************************************/
+
+	/**
+	 * Plots markers on the given map by sending an AJAX request to the server to get the form entries
+	 * @param {google.maps.Map} map - The map object
+	 */
+	function plotMarkers(map) {
+		// Get the jQuery element the map is attached to
 		let $mapElement = $(map.getDiv());
 
-		let allowsFeedback = $mapElement.data("feedback-active");
-
-		if (allowsFeedback != true) {
+		let formId = $mapElement.data("form-id");
+		if (!formId) {
 			return;
 		}
 
-		let feedbackButton = $mapElement
-			.closest(".feedback-map-wrapper")
-			.find(".share-feedback-button");
+		let ajaxData = {
+			action: "feedback_map_entries",
+			formId: formId,
+			nonce: feedbackMapsParams.nonce,
+		};
 
-		let addMarkerControls = $mapElement
-			.closest(".feedback-map-wrapper")
-			.find(".add-marker-controls");
+		// Get form entries using AJAX
+		$.ajax({
+			type: "GET",
+			url: feedbackMapsParams.ajaxUrl,
+			data: ajaxData,
+			dataType: "json",
+			success: function (response, textStatus, xhr) {
+				let formEntries = response.data;
+				if (formEntries.length) {
+					formEntries.forEach(function (entry) {
+						let marker = addMarker(map, entry);
 
-		if (!feedbackButton.length) {
-			return;
-		}
-
-		feedbackButton.on("click", function () {
-			if (map.infowindow) {
-				map.infowindow.close();
-			}
-
-			addMarkerControls.fadeIn(150);
-			addMarkerControls.attr("aria-hidden", "false");
-
-			feedbackButton.fadeOut(150);
-			feedbackButton.attr("aria-expanded", "true");
-
-			map.hasActiveModal = true;
-		});
-
-		let feedbackButtonOpener = $mapElement
-			.closest(".feedback-map-wrapper")
-			.find(".open-feedback-modal");
-
-		feedbackButtonOpener.on("click", function () {
-			let latLng = map.getCenter();
-			let $form = $mapElement.closest(".feedback-map-wrapper").find("form");
-			let $latInput = $form.find(".gfield.latitude input");
-			let $lngInput = $form.find(".gfield.longitude input");
-
-			$latInput.val(latLng.lat());
-			$lngInput.val(latLng.lng());
-		});
-
-		let cancelButton = $mapElement
-			.closest(".feedback-map-wrapper")
-			.find(".cancel");
-
-		cancelButton.on("click", function () {
-			addMarkerControls.fadeOut(150);
-			addMarkerControls.attr("aria-hidden", "true");
-			feedbackButton.fadeIn(150);
-			feedbackButton.attr("aria-expanded", "false");
-
-			map.hasActiveModal = false;
+						// Add the marker to the map object
+						if (!map.markers) {
+							map.markers = [];
+						}
+						map.markers.push(marker);
+					});
+				}
+			},
+			error: function (error) {
+				// NO ENTRIES FOUND
+				// console.error(error);
+			},
 		});
 	}
 
 	/**
-	 * Shows the UI elements on a map
+	 * Adds a marker to the map
 	 * @param {google.maps.Map} map - The map object
+	 * @param {Object} markerData - The data for the marker
+	 * @returns {google.maps.Marker} - The marker object
 	 */
-	function showMapInterface(map) {
-		map.setOptions({
-			mapTypeControl: true,
-			mapTypeControlOptions: {
-				mapTypeIds: ["roadmap", "satellite"],
-				style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-			},
-			zoomControl: true,
-			zoomControlOptions: {
-				position: google.maps.ControlPosition.RIGHT_TOP,
-			},
+	function addMarker(map, markerData) {
+		var latLng = {
+			lat: parseFloat(markerData.lat),
+			lng: parseFloat(markerData.lng),
+		};
+
+		let markerProps = {
+			position: latLng,
+			map: map,
+			// TODO: ICON OVERRIDE WOULD BE NICE
+		};
+
+		let $mapElement = $(map.getDiv());
+
+		// If the map has a filter, get the relevant field, and add the attributes to the marker
+		let hasFilter = $mapElement.data("filterby");
+		if (hasFilter) {
+			let value = markerData.fields[hasFilter].value;
+			if (value !== "") {
+				// TODO: CHECK WHAT HAPPENS IF ARRAY - EG CHECKBOX
+				// TODO: IN HERE WE WOULD CHANGE THE COLOR OF THE MARKER TOO BASED ON THE FILTER
+				markerProps.filter = value;
+			}
+		}
+
+		// TODO: Move to https://developers.google.com/maps/documentation/javascript/reference/advanced-markers#AdvancedMarkerElement
+		var marker = new google.maps.Marker(markerProps);
+
+		// Show the marker content when clicked
+		google.maps.event.addListener(marker, "click", function () {
+			if (!map.hasActiveModal) {
+				map.infowindow.setOptions({
+					content: generateMarkerContent(markerData),
+				});
+				map.infowindow.open(map, marker);
+
+				map.setCenter(marker.getPosition());
+			}
 		});
 
-		return map;
+		return marker;
 	}
 
 	/**
@@ -158,72 +172,167 @@
 		return content;
 	}
 
+	/********************************************
+	 * User Interface
+	 ********************************************/
+
 	/**
-	 * Adds a marker to the map
+	 * Shows the UI elements on a map
 	 * @param {google.maps.Map} map - The map object
-	 * @param {Object} markerData - The data for the marker
-	 * @returns  {google.maps.Marker} - The marker object
 	 */
-	function addMarker(map, markerData) {
-		var latLng = {
-			lat: parseFloat(markerData.lat),
-			lng: parseFloat(markerData.lng),
-		};
-
-		// TODO: Move to https://developers.google.com/maps/documentation/javascript/reference/advanced-markers#AdvancedMarkerElement
-		var marker = new google.maps.Marker({
-			position: latLng,
-			map: map,
-			// TODO: ICON OVERRIDE WOULD BE NICE
+	function showMapInterface(map) {
+		map.setOptions({
+			mapTypeControl: true,
+			mapTypeControlOptions: {
+				mapTypeIds: ["roadmap", "satellite"],
+				style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+			},
+			zoomControl: true,
+			zoomControlOptions: {
+				position: google.maps.ControlPosition.RIGHT_BOTTOM,
+			},
 		});
 
-		// Show the marker content when clicked
-		// TODO: CENTER MAP ON MARKER
-		google.maps.event.addListener(marker, "click", function () {
-			if (!map.hasActiveModal) {
-				map.infowindow.setOptions({
-					content: generateMarkerContent(markerData),
-				});
-				map.infowindow.open(map, marker);
-			}
-		});
-
-		return marker;
+		return map;
 	}
 
-	function plotMarkers(map) {
-		// Get the jQuery element the map is attached to
-		let $mapElement = $(map.getDiv());
+	/********************************************
+	 * Filter markers
+	 ********************************************/
 
-		let formId = $mapElement.data("form-id");
-		if (!formId) {
+	/**
+	 * Adds filtering to the map based on the data-filterby attribute
+	 * @param {google.maps.Map} map - The map object
+	 * @returns {void}
+	 */
+	function addFiltering(map) {
+		let $mapElement = $(map.getDiv());
+		let allowsFiltering = $mapElement.data("filterby");
+
+		if (!allowsFiltering) {
 			return;
 		}
 
-		// Get form entries using AJAX
-		$.ajax({
-			type: "GET",
-			url: feedbackMapsParams.ajaxUrl,
-			data: {
-				action: "feedback_map_entries",
-				formId: formId,
-				nonce: feedbackMapsParams.nonce,
-			},
-			dataType: "json",
-			success: function (response, textStatus, xhr) {
-				let formEntries = response.data;
-				if (formEntries.length) {
-					formEntries.forEach(function (entry) {
-						addMarker(map, entry);
-					});
-				}
-			},
-			error: function (error) {
-				// NO ENTRIES FOUND
-				// console.error(error);
-			},
+		let filterForm = $mapElement
+			.closest(".feedback-map-wrapper")
+			.find(".feedback-map-filters");
+
+		let filterInputs = filterForm.find("input[type=checkbox]");
+		// Add event listener to filter inputs to show/hide markers when checked
+		filterInputs.on("change", function () {
+			// hide any open infowindows
+			if (map.infowindow) {
+				map.infowindow.close();
+			}
+
+			// Update active Map Filters to match the checked inputs
+			let checkedFilters = filterInputs
+				.filter(":checked")
+				.map(function () {
+					return $(this).val();
+				})
+				.toArray();
+			map.activeFilters = checkedFilters;
+
+			// Determine whether or not to show each marker based on the active filters
+			if (map.activeFilters.length > 0) {
+				map.markers.forEach(function (marker) {
+					if (marker.filter) {
+						if (map.activeFilters.includes(marker.filter)) {
+							marker.setVisible(true);
+						} else {
+							marker.setVisible(false);
+						}
+					} else {
+						marker.setVisible(false);
+					}
+				});
+			} else {
+				// No filters => show all markers
+				map.markers.forEach(function (marker) {
+					marker.setVisible(true);
+				});
+			}
 		});
 	}
+
+	/********************************************
+	 * USER FEEDBACK
+	 ********************************************/
+
+	/**
+	 * Adds user feedback functionality to the map
+	 * @param {google.maps.Map} map - The map object
+	 * @returns {void}
+	 */
+	function addUserFeedback(map) {
+		let $mapElement = $(map.getDiv());
+
+		let allowsFeedback = $mapElement.data("feedback-active");
+
+		if (allowsFeedback != true) {
+			return;
+		}
+
+		let feedbackButton = $mapElement
+			.closest(".feedback-map-wrapper")
+			.find(".share-feedback-button");
+
+		let addMarkerControls = $mapElement
+			.closest(".feedback-map-wrapper")
+			.find(".add-marker-controls");
+
+		if (!feedbackButton.length) {
+			return;
+		}
+
+		feedbackButton.on("click", function () {
+			if (map.infowindow) {
+				map.infowindow.close();
+			}
+			feedbackButton.fadeOut(150);
+			feedbackButton.attr("aria-expanded", "true");
+
+			setTimeout(() => {
+				addMarkerControls.fadeIn(150);
+				addMarkerControls.attr("aria-hidden", "false");
+				map.hasActiveModal = true;
+			}, 200);
+		});
+
+		let feedbackButtonOpener = $mapElement
+			.closest(".feedback-map-wrapper")
+			.find(".open-feedback-modal");
+
+		feedbackButtonOpener.on("click", function () {
+			let latLng = map.getCenter();
+			let $form = $mapElement.closest(".feedback-map-wrapper").find("form");
+			let $latInput = $form.find(".gfield.latitude input");
+			let $lngInput = $form.find(".gfield.longitude input");
+
+			$latInput.val(latLng.lat());
+			$lngInput.val(latLng.lng());
+		});
+
+		let cancelButton = $mapElement
+			.closest(".feedback-map-wrapper")
+			.find(".cancel");
+
+		cancelButton.on("click", function () {
+			addMarkerControls.fadeOut(300);
+			addMarkerControls.attr("aria-hidden", "true");
+
+			setTimeout(() => {
+				feedbackButton.fadeIn(50);
+				feedbackButton.attr("aria-expanded", "false");
+				map.hasActiveModal = false;
+			}, 350);
+		});
+	}
+
+	/********************************************
+	 * INITIALISE MAPS
+	 ********************************************/
 
 	/**
 	 * Initialises a single feedback map using the Google Maps Javascript API
@@ -232,7 +341,7 @@
 	 * @param {*} $mapElement - jQuery object of the map element
 	 * @returns {google.maps.Map} - The map object
 	 */
-	async function initialiseFeedbackMap($mapElement) {
+	async function initialiseSingleMap($mapElement) {
 		let map;
 		let mapZoom = $mapElement.data("zoom") ? $mapElement.data("zoom") : 15;
 		let minMapZoom = mapZoom - 3 < 12 ? 12 : mapZoom - 3; // Dont let min Zoom go below 12
@@ -282,13 +391,18 @@
 			}
 		}
 
+		// Wait for the map to be initialised
 		await initMap();
 
+		// Populate the map with markers and add UI elements
 		showMapInterface(map);
 		plotMarkers(map);
 		addUserFeedback(map);
+		addFiltering(map);
 
+		// Set up an infoWindow and activeFilters property on the map object
 		map.infowindow = new google.maps.InfoWindow();
+		map.activeFilters = {};
 
 		return map;
 	}
@@ -297,7 +411,7 @@
 	 * Loops through all feedback maps on the page and initialises them via IntersectionObserver or straight away if not supported
 	 * @returns {void}
 	 */
-	function initialiseFeedbackMaps() {
+	function initialiseAllMaps() {
 		var feedbackMaps = [].slice.call(
 			document.querySelectorAll(".feedback-map")
 		);
@@ -312,7 +426,7 @@
 				function (entries, observer) {
 					entries.forEach(function (entry) {
 						if (entry.isIntersecting) {
-							var map = initialiseFeedbackMap($(entry.target));
+							var map = initialiseSingleMap($(entry.target));
 							observeFeedbackMaps.unobserve(entry.target);
 						}
 					});
@@ -327,7 +441,7 @@
 		} else {
 			// For browsers that don't support intersection observer, load all images straight away
 			feedbackMaps.forEach(function (element) {
-				var map = initialiseFeedbackMap($(element));
+				var map = initialiseSingleMap($(element));
 			});
 		}
 	}
@@ -340,14 +454,14 @@
 				// FIXME: THIS IS NOT INITIALISING THE MAP ON THE EDITOR
 				console.log($elem, blockDetails);
 				if ($elem[0].innerHTML.includes("feedback-map")) {
-					initialiseFeedbackMaps();
+					initialiseAllMaps();
 				}
 			}
 		);
 	} else {
 		// Otherwise run on DOMContentLoaded for the front end
 		document.addEventListener("DOMContentLoaded", function () {
-			initialiseFeedbackMaps();
+			initialiseAllMaps();
 		});
 	}
 })(jQuery);
--- blocks/feedback-map/view.php
+++ blocks/feedback-map/view.php
@@ -54,6 +54,8 @@ $block_classlist = implode(' ', $block_classlist);
       'data-lng' => $fields['longitude'],
     ];
 
+    $fields['overlay_source'] = 'https://epls.design/dev/test.kml';
+
     if ($fields['zoom']) $map_attributes['data-zoom'] = $fields['zoom'];
     if ($fields['gravity_form_id']) $map_attributes['data-form-id'] = $fields['gravity_form_id'];
     if ($fields['overlay_source'])  $map_attributes['data-overlay-source'] = $fields['overlay_source'];
@@ -63,6 +65,22 @@ $block_classlist = implode(' ', $block_classlist);
     if ($fields['feedback_active']) $map_attributes['data-feedback-active'] = 'true';
     else $map_attributes['data-feedback-active'] = 'false';
 
+    $allow_filtering = false;
+    if ($fields['filter_by_field']) {
+      // Check if the field ID given is of type 'Select', 'Checkbox' or 'Radio' and if so, build a filter
+      $form = GFAPI::get_form($fields['gravity_form_id']);
+      $field_to_filter_by = GFAPI::get_field($form, $fields['filter_by_field']);
+
+      $accepted_types = ['select', 'checkbox', 'radio'];
+
+      if (!in_array($field_to_filter_by->type, $accepted_types)) {
+        return false;
+      } else {
+        $map_attributes['data-filterby'] = $fields['filter_by_field'];
+        $allow_filtering = true;
+      }
+    }
+
     if (!empty($map_attributes)) {
       $map_attrs = '';
       foreach ($map_attributes as $key => $value) {
@@ -70,45 +88,58 @@ $block_classlist = implode(' ', $block_classlist);
       }
     }
 
+    // TODO: Find a nice way to change marker colors and icons. Pass through as attributes? What about when its filtered
+
     // TODO: Add an aria suitable fallback for the map
 
   ?>
 
-    <div class="feedback-map-wrapper">
-      <div class="feedback-map" <?php echo $map_attrs; ?>></div>
+  <div class="feedback-map-wrapper">
+    <div class="feedback-map" <?php echo $map_attrs; ?>></div>
 
-      <?php if ($fields['feedback_active']) { ?>
+    <?php if ($allow_filtering) { ?>
+    <form class="feedback-map-filters" aria-label="<?php _e('Filter feedback', 'jellypress'); ?>">
+      <?php
+          foreach ($field_to_filter_by->choices as $choice) {
+            $filter_id = sanitize_title($choice['value']);
+            echo '<label class="feedback-map-filter" for="' . $filter_id . '"><input type="checkbox" name="filter-map-' . $fields['gravity_form_id'] . '[]" value="' . $choice['value'] . '" id="' . $filter_id . '">' . $choice['text'] . '</label>';
+          }
+          ?>
+    </form>
+    <?php } ?>
 
-        <button class="button success share-feedback-button small" aria-controls="<?php echo 'marker-add-' . $fields['gravity_form_id']; ?>" aria-expanded="false"><?php _e('Share your feedback', 'jellypress'); ?></button>
-        <div class="add-marker-controls" id="marker-add-<?php echo $fields['gravity_form_id']; ?>" style="display:none;">
-          <span class="crosshair">
-            <span class="vertical"></span>
-            <span class="horizontal"></span>
-          </span>
-          <div class="description">
-            <p><?php _e('Position the crosshair on the location where you would like to add feedback, and then click on "Add feedback here"', 'jellypress'); ?></p>
-            <div class="button-list">
-              <button class="button xsmall success open-feedback-modal" onclick="toggleModal('add-feedback-<?php echo $fields['gravity_form_id']; ?>');">
-                <?php _e('Add feedback here', 'jellypress'); ?>
-              </button>
-              <button class="button ghost white xsmall cancel">
-                <?php _e('Cancel', 'jellypress'); ?>
-              </button>
-            </div>
-          </div>
+    <?php if ($fields['feedback_active']) { ?>
+
+    <button class=" button success share-feedback-button small" aria-controls="<?php echo 'marker-add-' . $fields['gravity_form_id']; ?>" aria-expanded="false"><?php _e('Share your feedback', 'jellypress'); ?></button>
+    <div class="add-marker-controls" id="marker-add-<?php echo $fields['gravity_form_id']; ?>" style="display:none;">
+      <span class="crosshair">
+        <span class="vertical"></span>
+        <span class="horizontal"></span>
+      </span>
+      <div class="description">
+        <p><?php _e('Position the crosshair on the location where you would like to add feedback, and then click on "Add feedback here"', 'jellypress'); ?></p>
+        <div class="button-list">
+          <button class="button xsmall success open-feedback-modal" onclick="toggleModal('add-feedback-<?php echo $fields['gravity_form_id']; ?>');">
+            <?php _e('Add feedback here', 'jellypress'); ?>
+          </button>
+          <button class="button ghost white xsmall cancel">
+            <?php _e('Cancel', 'jellypress'); ?>
+          </button>
         </div>
-
-        <dialog class="modal bg-white feedback-modal" id="add-feedback-<?php echo $fields['gravity_form_id']; ?>">
-          <div class="modal-content">
-            <?php
-            echo do_shortcode('[gravityform id="' . $fields['gravity_form_id'] . '" title="false"  ajax="true"]')
-            ?>
-          </div>
-        </dialog>
-
-      <?php } ?>
-
+      </div>
     </div>
+
+    <dialog class="modal bg-white feedback-modal" id="add-feedback-<?php echo $fields['gravity_form_id']; ?>">
+      <div class="modal-content">
+        <?php
+            echo do_shortcode('[gravityform id="' . $fields['gravity_form_id'] . '" title="false" description="false"  ajax="true"]')
+            ?>
+      </div>
+    </dialog>
+
+    <?php } ?>
+
+  </div>
 
   <?php
   }
